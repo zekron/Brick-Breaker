69160b65bd86759bc4b59722308ddc19c4400560
Zikzik <34153457+zekron@users.noreply.github.com>
2021-06-26 18:11:33 +0800

    Add ScoreBoard.scene to SceneInBuild

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -57,8 +57,8 @@ public class MainManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                //SceneLoader.LoadScene("");
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                SceneLoader.LoadScene("ScoreBoard");
             }
         }
     }
